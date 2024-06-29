c6834030cbfc5b20e05876bd73a99b0af1b2a66c
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-06-29 16:39:58 -0600

    passed class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,53 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists(artist)    
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+    
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else: 
+            Song.artist_count[self.artist] = 1
+
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+
+print(ninety_nine_problems.name)  # "99 Problems"
+print(ninety_nine_problems.artist)  # "Jay-Z"
+print(ninety_nine_problems.genre)  # "Rap"
+
+print(Song.count)  # 1
+print(Song.genres)  # ["Rap"]
+print(Song.artists)  # ["Jay-Z"]
+print(Song.genre_count)  # {"Rap": 1}
+print(Song.artist_count)  # {"Jay-Z": 1}
